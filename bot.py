eda1c61cea10d899bdd204cc47642c7a5c77b4af
Marissa <marissa.wagner@littles.moe>
2019-11-07 09:34:49 +0000

    Send message to specified chat id

--- bot.py
+++ bot.py
@@ -1,1 +1,23 @@
-print("Hello")
+# Import settings
+import env
+
+# Import important modules
+import sys
+from telethon import TelegramClient,events
+
+# parse account from cli
+if len(sys.argv) != 3:
+	print("You need to specify an account name and a phone number!")
+	exit()
+
+account_file = sys.argv[1]
+telephone_number = sys.argv[2]
+	
+client = TelegramClient(account_file, env.api_id, env.api_hash)
+						
+async def main():
+    # Now you can use all client methods listed below, like for example...
+    await client.send_message(env.chat_id, 'owo')
+
+with client:
+    client.loop.run_until_complete(main())
